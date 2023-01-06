b2808900a0a35b3890d36baffaac3519e67f01f7
Jim Romaine <Jim.Romaine@healthresearch.org>
2023-01-05 20:26:42 -0500

    rebuild for base path

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,9 +24,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   publicRuntimeConfig: {
@@ -35,8 +33,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -67,11 +64,10 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    // baseURL: '/',
+    baseURL: 'https://jimmerydad.github.io/cat-quotes/',
     // Do away with the baseUrl when using proxy
-    proxy: true,
+    // proxy: true,
     credentials: false
-
   },
 
   proxy: {
@@ -110,8 +106,8 @@ export default {
   build: {
     publicPath: '/nuxt/',
     /*
-                       ** You can extend webpack config here
-                       */
+                     ** You can extend webpack config here
+                     */
     extend (config, ctx) { }
   }
 }
